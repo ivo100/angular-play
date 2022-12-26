5696f55c4e2bdfb8da4d3b2a09a1e0cd0e03ee77
Ivo Stoyanov <ivo.stoyanov@groundswell-giving.io>
2022-12-26 08:34:30 -0800

    simple

--- simple/src/app/app.component.ts
+++ simple/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-/** @title Select with 2-way value binding */
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'simple';
-  selected = 'option2';
 }
+
+
