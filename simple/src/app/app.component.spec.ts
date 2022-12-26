0413a140dc234834e7bb564f2c65e6b7e1a878e9
Ivo Stoyanov <ivo.stoyanov@groundswell-giving.io>
2022-12-25 18:18:52 -0800

    form

--- simple/src/app/app.component.spec.ts
+++ simple/src/app/app.component.spec.ts
@@ -22,10 +22,11 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('simple');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Hello simple');
-  });
+  // it('should render toggle', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('.content span')?.textContent).toContain('Hello simple');
+  // });
+
 });
--- simple/src/app/app.module.ts
+++ simple/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
