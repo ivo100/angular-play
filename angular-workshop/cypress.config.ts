75ef9ef1b2966058acbe690962313638c4f93c38
Ivo Stoyanov <ivo.stoyanov@groundswell-giving.io>
2022-12-27 12:53:56 -0800

    wip

--- angular-workshop/cypress.config.ts
+++ angular-workshop/cypress.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:4200',
+    baseUrl: "http://localhost:4200",
     video: false,
+  },
+
+  component: {
+    devServer: {
+      framework: "angular",
+      bundler: "webpack",
+    },
+    specPattern: "**/*.cy.ts",
   },
 });
