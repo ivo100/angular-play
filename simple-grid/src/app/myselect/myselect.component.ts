0e2ecf64b43d3facfbffe87795ac8c2f2be80264
Ivo Stoyanov <ivo@skyway-consulting.com>
2023-01-17 08:08:55 -0800

    grid

---
+++ simple-grid/src/app/myselect/myselect.component.ts
@@ -0,0 +1,45 @@
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {MatInput} from "@angular/material/input";
+
+@Component({
+  selector: 'app-myselect',
+  templateUrl: './myselect.component.html',
+  styleUrls: ['./myselect.component.css']
+})
+
+
+export class MyselectComponent implements OnInit {
+
+  //@ViewChild('input') myinput: MatInput;
+  //@ViewChild(MatInput) myinput;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  title = 'simple';
+  private _selected = '';
+  private _disabled = 'true';
+
+  @Input()
+  get disabled (): string {
+    return this._disabled;
+  }
+  set disabled (val: string) {
+    console.log("set disabled", val);
+    this._disabled = val;
+  }
+
+  @Input()
+  get selected (): string {
+    return this._selected;
+  }
+  set selected (val: string) {
+    console.log("set selected", val);
+    this._disabled = val ? "false" : "true";
+    this._selected = val;
+    //this.myinput.focus();
+  }
+
+}
