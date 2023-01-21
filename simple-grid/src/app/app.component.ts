82dc7d073dad960ac29c9c343ac8f01697441364
Ivo Stoyanov <ivo@skyway-consulting.com>
2023-01-21 14:19:03 -0800

    grid

--- simple-grid/src/app/app.component.ts
+++ simple-grid/src/app/app.component.ts
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+  columnDefs = [{ field: "make" }, { field: "model" }, { field: "price" }];
+
+  rowData = [
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxter", price: 72000 }
+  ];
 }
 
 
--- simple-grid/src/app/app.module.ts
+++ simple-grid/src/app/app.module.ts
@@ -6,13 +6,19 @@ import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MyselectComponent } from './myselect/myselect.component';
+import { FormsModule } from '@angular/forms';
+import { AgGridModule } from 'ag-grid-angular';
+
 @NgModule({
   declarations: [
     AppComponent,
     MyselectComponent
   ],
+
   imports: [
     BrowserModule,
+    FormsModule,
+    AgGridModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatSelectModule,
